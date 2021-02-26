14c427e130cb1c22fdda4bdf96a8d70b5f05d591
Mike Ruane <mikeruanedev@gmail.com>
2021-02-25 19:42:02 -0500

    set up file, index.js psuedocode

---
+++ Develop/index.js
@@ -0,0 +1,29 @@
+// TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require("fs");
+
+// TODO: Create an array of questions for user input
+const questions = [];
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {
+
+}
+
+// TODO: Create a function to initialize app
+function init() {
+    
+    //project title
+    //description
+    //installation instructions
+    //usage information
+    //countribution guidelines
+    //test instructions
+    //choose licence
+    //github user name
+    //email address
+
+}
+
+// Function call to initialize app
+init();
