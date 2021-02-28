a70bbf601ed2ba61f35bbad0025389c157badc93
Mike Ruane <mikeruanedev@gmail.com>
2021-02-28 16:21:56 -0500

    go back to program strucutre that asks in-line

--- Develop/index.js
+++ Develop/index.js
@@ -2,107 +2,95 @@
 const inquirer = require('inquirer');
 const fs = require("fs");
 
-// TODO: Create an array of questions for user input
-function questionData(fieldName, isRequired, questionsSsv, optionsSsv = "") {
-    this.fieldName = fieldName;
-    this.isRequired = isRequired;
-    this.questionsSsv = questionsSsv;
-    this.optionsSsv = optionsSsv;
-  
-    this.questions = this.questionsSsv.split(";");
-    if (this.questions === undefined) {
-      this.questions = [];
-    }
-    this.options = this.optionsSsv.split(";");
-    if (this.options === undefined) {
-      this.options = [];
-    }
-}
-
-function matchData(choiceName, pathOrTemplate) {
-    this.choiceName = choiceName;
-    this.pathOrTemplate =  pathOrTemplate;
-}
-
-//promptSession
-function promptSession(questionDataList, matchDataList) {
-  let readmeText = "";
-  let qDataIndex = 0;
-  let questionIndex = 0;
-
-  const askQuestion = () => {
-
-    let qData = questionDataList[qDataIndex];
-
-      inquirer.prompt([
-      {
-          name: "answer", 
-          message: qData.questions[0]
-      }
-      ]).then((response) => {
-          readmeText += response.answer + '\r\n';
-          qDataIndex++;
-          // limit recursion to item count of qdl
-          if (qDataIndex < questionDataList.length) {
-            // Ask the next question
-            askQuestion ();
-          }
-          else {
-            readmeText += response.answer + '\r\n';
-            console.log(readmeText);
-            return;
-          }
-      });
-
-  };        
-  askQuestion();                                                                                                            
-}    
-
-
-
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {
-
-}
-
 // TODO: Create a function to initialize app
 function init() {
+      //read & split base template text
+      const baseTemplateText = fs.readFileSync("./data/baseTemplate.txt", "utf8");
+      const readmeSections = baseTemplateText.split("~");
   
-  let questionDataList = [
-    new questionData("projectTitle", true, "what is your project's title?"),
-    new questionData("description", true, "enter a brief description of your application"),
-    new questionData("installInstructions", false, "use codeblock (y/n)?;enter installation instructions"),
-    new questionData("usageInformation", false, "use codeblock (y/n)?;enter usage information", ""),
-    new questionData("contributionInfo", false, "list some guidelines for contribution to the project"),
-    new questionData("testInstructions", false, "provide some instructions for testing"),
-    new questionData("licence", false, "choose a licence", "> MIT;> Boost;> Apache"),
-    new questionData("gitHubUsername", true, "what is your username on GitHub?"),
-    new questionData("email", true, "what is your email address?")
-  ];
-
-    //read & split base template text
-    const baseTemplateText = fs.readFileSync("./data/baseTemplate.txt", "utf8");
-    const readmeSections = baseTemplateText.split("~");
-
-    //add the text of each document section to its matching question
-    for (let i = 0; i < questionDataList.length; i++) {
-      let qData = questionDataList[i];
-      qData["docSectionText"] = readmeSections[i];
-    }
-
-    //create matchData map for special section formatting
-    const matchDataList = [
-      new matchData("> MIT", "./data/mitLicence.txt"),
-      new matchData("> Boost", "./data/boostLicence.txt"),
-      new matchData("> Apache","./data/apacheLicence.txt" )
-    ];
-
-    //const session = new promptSession(questionDataList, matchDataList);
-    //console.log(session);
-
-    //session.run();
-
-    promptSession(questionDataList, matchDataList);
+      inquirer
+    .prompt([
+      {
+          type: "input",
+          message: "what is your project's title?",
+          name: "projectTitle",
+        },
+        {
+          type: "input",
+          message: "provide a brief description of your application",
+          name: "description",
+        },
+        {
+          type: "input",
+          message: "enter installation instructions",
+          name: "installInstructions",
+        }, 
+        {
+          type: "input",
+          message: "enter usage information",
+          name: "usageInformation",
+        }, 
+        {
+          type: "input",
+          message: "list some guidelines for contribution to the project",
+          name: "contributionInfo",
+        }, 
+        {
+          type: "input",
+          message: "provide some instructions for testing",
+          name: "testInstructions",
+        }, 
+        {
+          type: "rawlist",
+          message: "choose licence",
+          name: "licence",
+          choices: ["Mit", "Apache", "Boost"]
+        }, 
+        {
+          type: "input",
+          message: "what's the name of your GitHub account?",
+          name: "gitHubUsername",
+        }, 
+        {
+          type: "input",
+          message: "what's your email address?",
+          name: "email",
+        }, 
+    ])
+    .then((response) => {    
+      let readmeText = "";
+      readmeText += readmeSections[0].replace("[projectTitle]", response.projectTitle);
+      readmeText += readmeSections[1].replace("[description]", response.description);
+      readmeText += readmeSections[2].replace("[installInstructions]", response.installInstructions);
+      readmeText += readmeSections[3].replace("[usageInformation]", response.usageInformation);
+      readmeText += readmeSections[4].replace("[contributionInfo]", response.contributionInfo);
+      readmeText += readmeSections[5].replace("[testInstructions]", response.testInstructions);
+  
+      const licenceChoice = readmeSections[6].replace("[licence]", response.licence);
+      let licenceText = "";
+  
+      if (licenceChoice === "Mit") {
+          licenceText = fs.readFileSync("./data/mitLicence.txt", "utf8");
+      }
+      else if (licenceChoice === "Boost") {
+          licenceText = fs.readFileSync("./data/boostLicence.txt", "utf8");
+      }
+      else {
+          licenceText = fs.readFileSync("./data/apacheLicence.txt", "utf8");
+      }
+  
+      readmeText += readmeSections[6].replace("licence", licenceText);
+  
+      readmeText += readmeSections[7].replace("[gitHubUsername]", response.gitHubUsername);
+      readmeText += readmeSections[8].replace("[email]", response.email);
+  
+      //remove base template delimiters
+      readmeText = readmeText.replace("~", "");
+  
+      //test output
+      fs.writeFileSync("./readmeOut.md", readmeText);
+  
+    });
 }
 
 // Function call to initialize app
